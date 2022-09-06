8618565e9353b692895a76743642d2d99394d9ff
YNGRID VITORIA SA BAETA <u21694@academico.cotuca.unicamp.br>
2022-09-06 08:08:19 -0300

    atualizações 😋

--- app/src/main/java/com/example/recycleviewproject/MeuAdaptador.java
+++ app/src/main/java/com/example/recycleviewproject/MeuAdaptador.java
@@ -1,5 +1,6 @@
 package com.example.recycleviewproject;
 
+import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,44 +16,59 @@ import java.util.List;
 //MyHolder é um tipo, esta criando um adaptador de nome, email e o lixinho
  class MeuAdaptador extends RecyclerView.Adapter<MeuAdaptador.MyHolder> {
 
-     //defina uma lista de usuario
-     List<Usuario> listaUsuario = new ArrayList<>(); //cria uma listaUsuario
+    //defina uma lista de usuario
+    List<Usuario> listaUsuario = new ArrayList<>(); //cria uma listaUsuario
 
-     ItemClickListener itemClickListener;
+    //clicar no item e pegar o evento dele
+    ItemClickListener itemClickListener;
 
-     //construtor da classe
-     public MeuAdaptador(List<Usuario> list){
-         this.listaUsuario = list; //passa a listaUsuario para o parametro do construtor
-     }
+    //construtor da classe
+    public MeuAdaptador(List<Usuario> list) {
+        this.listaUsuario = list; //passa a listaUsuario para o parametro do construtor
+    }
 
-     @NonNull
-     @Override
-     public MeuAdaptador.MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row, parent, false);
-         return new MyHolder(view);
-     }
+    @NonNull
+    @Override
+    //para podermos criar uma view
+    public MeuAdaptador.MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //um objeto do tipo view, vai ser o item row do xml, que tem o nome, email e lixo do xml
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row, parent, false);
+        return new MyHolder(view);
+    }
 
-     @Override
-     public void onBindViewHolder(@NonNull MeuAdaptador.MyHolder holder, int position) {
+    @Override
+    public void onBindViewHolder(@NonNull MeuAdaptador.MyHolder holder, @SuppressLint("RecyclerView") int position) {
+        final Usuario userData = listaUsuario.get(position);
 
-     }
+        holder.tvNomeItem.setText(userData.getNome());
+        holder.tvEmailItem.setText(userData.getEmail());
 
-     @Override
-     public int getItemCount() {
-         return 0;
-     }
+        holder.itemView.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                itemClickListener.OnItemClick(position, userData);
+            }
+        }));
+    }
 
-     public static class MyHolder extends RecyclerView.ViewHolder{
-         TextView tvNomeItem, tvEmailItem, tvDeleteItem;
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
-         public MyHolder(View itemView){
-             //super é o construtor da classe que esta herdando
-             super(itemView); //esta mandando o itemView pra cá
+    public static class MyHolder extends RecyclerView.ViewHolder {
+        TextView tvNomeItem, tvEmailItem, tvDeleteItem;
 
-             tvNomeItem = itemView.findViewById(R.id.tvNomeItem);
-             tvEmailItem = itemView.findViewById(R.id.tvEmailItem);
-             tvDeleteItem = itemView.findViewById(R.id.tvDeleteItem);
+        public MyHolder(View itemView) {
+            //super é o construtor da classe que esta herdando
+            super(itemView); //esta mandando o itemView pra cá
 
-         }
-     }
+            tvNomeItem = itemView.findViewById(R.id.tvNomeItem);
+            tvEmailItem = itemView.findViewById(R.id.tvEmailItem);
+            tvDeleteItem = itemView.findViewById(R.id.tvDeleteItem);
+
+        }
+    }
+
+    public void updateVazio(){}
  }
